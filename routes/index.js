9e54e16b4160e15e19239353a3748d717cbf77eb
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2020-07-02 21:28:31 -0400

    Add project files.

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var articlesModel = require('../models/articles');
+/* GET home page. */
+router.get('/', function (req, res) {
+    try {
+        //Create a new article using the Articles Model Schema
+        const article = new articlesModel({ name: "New article", description: "New description" });
+        //Insert article into DB
+        article.save();
+        //Retrieve all articles if there is any 
+        articlesModel.find({}, function (err,foundArticles) {
+            console.log(err);
+            console.log(foundArticles);
+            //Pass found articles from server to pug file
+            res.render('index', { articles: foundArticles });
+        });
+    } catch (err) {
+        console.log(err);
+        res.render('index', { title: 'Express' });
+    }
+});
+
+module.exports = router;
